c6b832e6602d01c6ee444a47057527e4f7bf2f25
Alesialoco <125814881+Alesialoco@users.noreply.github.com>
2025-02-21 18:14:23 +0300

    Add code for task

---
+++ bin_search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int find_element(const std::vector<int>& masive, int need_elem){
+    int left = 0;
+    int right = masive.size() - 1;
+    while (left <= right){
+        int middle = (left + right) >> 1;
+        if (need_elem == masive[middle])
+            return middle;
+        else if (masive[middle] < need_elem){
+            if (masive[right] < need_elem)
+                right = middle - 1;
+            else
+                left = middle + 1;
+        }
+        else{
+            if (masive[right] > need_elem)
+                right = middle - 1;
+            else
+                left = middle + 1;
+        }
+    }
+    return -1;
+}
+
